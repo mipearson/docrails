2038ade78a7c897bf57379d74beabf77355189b2
Michael Pearson <mipearson@gmail.com>
2011-11-26 14:13:23 +1100

    Make kindle generation an option, not the default.

--- railties/guides/rails_guides/generator.rb
+++ railties/guides/rails_guides/generator.rb
@@ -73,21 +73,38 @@ module RailsGuides
 
     def initialize(output=nil)
       @lang = ENV['GUIDES_LANGUAGE']
+      @kindle = ENV['KINDLE']
+      check_for_kindlegen if kindle?
       initialize_dirs(output)
       create_output_dir_if_needed
       set_flags_from_environment
     end
 
+    def kindle?
+      @kindle
+    end
+    
     def generate
       generate_guides
       copy_assets
     end
 
     private
+    
+    def check_for_kindlegen
+      if `which kindlegen`.strip == ''
+        raise "Can't create a kindle version without `kindlegen`."
+      end
+    end
+      
+    def default_output_dir
+      File.join(@guides_dir, (kindle? ? ['output', 'kindle'] : 'output'), @lang.to_s)
+    end
+    
     def initialize_dirs(output)
       @guides_dir = File.join(File.dirname(__FILE__), '..')
       @source_dir = File.join(@guides_dir, "source", @lang.to_s)
-      @output_dir = output || File.join(@guides_dir, "output", @lang.to_s)
+      @output_dir = output || default_output_dir
     end
 
     def create_output_dir_if_needed
@@ -109,6 +126,11 @@ module RailsGuides
 
     def guides_to_generate
       guides = Dir.entries(source_dir).grep(GUIDES_RE)
+      if kindle?
+        Dir.entries(File.join(source_dir, 'kindle')).grep(GUIDES_RE).map do |entry|
+          guides << File.join('kindle', entry)
+        end
+      end
       ENV.key?('ONLY') ? select_only(guides) : guides
     end
 
@@ -131,15 +153,21 @@ module RailsGuides
       end
     end
     
+    def output_path_for(output_file)
+      File.join(output_dir, File.basename(output_file))
+    end
+    
     def generate?(source_file, output_file)
       fin  = File.join(source_dir, source_file)
-      fout = File.join(output_dir, output_file)
+      fout = output_path_for(output_file)
       all || !File.exists?(fout) || File.mtime(fout) < File.mtime(fin)
     end
-
+    
     def generate_guide(guide, output_file)
-      puts "Generating #{output_file}"
-      output_path = File.join(output_dir, output_file)
+      output_path = output_path_for(output_file)
+      puts "Generating #{guide} as #{output_file}"
+      use_layout = kindle? ? 'kindle/layout' : 'layout'
+      
       File.open(output_path, 'w') do |f|
         view = ActionView::Base.new(source_dir, :edge => edge)
         view.extend(Helpers)
@@ -147,13 +175,13 @@ module RailsGuides
         if guide =~ /\.(\w+)\.erb$/
           # Generate the special pages like the home.
           # Passing a template handler in the template name is deprecated. So pass the file name without the extension.
-          result = view.render(:layout => 'layout_kindle', :file => $`)
+          result = view.render(:layout => use_layout, :formats => [$1], :file => $`)
         else
           body = File.read(File.join(source_dir, guide))
           body = set_header_section(body, view)
           body = set_index(body, view)
 
-          result = view.render(:layout => 'layout_kindle', :text => textile(body))
+          result = view.render(:layout => use_layout, :text => textile(body))
 
           warn_about_broken_links(result) if @warnings
         end
